f30fa7aa0d952a466b61f13f9ebe5f4f045fdd46
MaksymY <maksym.yankivskyypro@gmail.com>
2019-04-04 20:38:27 +0200

    begin on administratror panel

---
+++ admin/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Lobster" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="../css/adminStyle.css">
+  <title>Maksym Food</title>
+</head>
+
+  <body>
+    <h1 class="text-logo"><span class="fas fa-utensils"></span> Maksym Food <span class="fas fa-utensils"></h1>
+    <div class="container admin">
+      <div class="row">
+        <h1><strong>Liste des items </strong><a href="insert.php" class="btn btn-success btn-lg"><span class="fas fa-plus"></span>Ajouter</a></h1>
+        <table class="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Nom</th>
+              <th>Description</th>
+              <th>Prix</th>
+              <th>Catégorie</th>
+              <th>Actions</th>
+            </th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Item 1</td>
+              <td>Description 1</td>
+              <td>Prix 1</td>
+              <td>Catégorie 1</td>
+              <td width="300">
+                <a class="btn btn-outline-dark" href="view.php?id=1"><span class="far fa-eye"></span> Voir</a>
+                <a class="btn btn-primary" href="update.php?id=1"><span class="fas fa-pen"></span> Modifier</a>
+                <a class="btn btn-danger" href="delete.php?id=1"><span class="far fa-trash-alt"></span> Supprimer</a>
+              </td>
+            </tr>
+            <tr>
+              <td>Item 2</td>
+              <td>Description 2</td>
+              <td>Prix 2</td>
+              <td>Catégorie 2</td>
+              <td width="300">
+                <a class="btn btn-outline-dark" href="view.php?id=2"><span class="far fa-eye"></span> Voir</a>
+                <a class="btn btn-primary" href="update.php?id=2"><span class="fas fa-pen"></span> Modifier</a>
+                <a class="btn btn-danger" href="delete.php?id=2"><span class="far fa-trash-alt"></span> Supprimer</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </body>
+</html>
