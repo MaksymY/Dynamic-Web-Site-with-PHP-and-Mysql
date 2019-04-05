136f718de6ca3aeef9571ae39cbeb2e50f89740e
MaksymY <maksym.yankivskyypro@gmail.com>
2019-04-05 20:37:34 +0200

    insert in php is ready

--- admin/insert.php
+++ admin/insert.php
@@ -3,6 +3,74 @@
 
   $nameError = $descriptionError = $priceError = $categoryError = $imageError = $name = $description = $price = $category = $image = "";
 
+  if(!empty($_POST)){
+      $name = checkInput($_POST['name']);
+      $description = checkInput($_POST['descriotion']);
+      $price = checkInput($_POST['price']);
+      $category = checkInput($_POST['category']);
+      $image = checkInput($_FILES['image']['name']);
+      $imagePath = '../images/' . basename($image);
+      $imageExtension = pathinfo($imagePath, PATHINFO_EXTENSION);
+      $isSuccess = true;
+      $isUploadSuccess = false;
+
+      if(empty($name)){
+          $nameError = 'Ce champ ne peut pas être vide';
+          $isSuccess = false;
+      }
+      if(empty($description)){
+        $descriptionError = 'Ce champ ne peut pas être vide';
+        $isSuccess = false;
+      }
+      if(empty($price)){
+        $priceError = 'Ce champ ne peut pas être vide';
+        $isSuccess = false;
+      }
+      if(empty($category)){
+        $categoryError = 'Ce champ ne peut pas être vide';
+        $isSuccess = false;
+      }
+      if(empty($image)){
+        $imageError = 'Ce champ ne peut pas être vide';
+        $isSuccess = false;
+      }else{
+            $isUploadSuccess = true;
+            if($imageExtension != "jpg" && $imageExtension !="png" && $imageExtension !="jpeg" && $imageExtension != "gif"){
+                $imageError="Les fichier autorises sont: .jpg, .jpeg, .png, .gif";
+                $isUploadSuccess = false;
+            }
+            if(file_exists($imagePath)){
+                $imageError="Le fichier existe deja";
+                $isUploadSuccess=false;
+            }
+            if($_FILES["image"]["size"] > 500000){
+                $imageError = "Le fichier ne doit pas depasser les 500KB";
+                $isUploadSuccess = false;
+            }
+            if($isUploadSuccess){
+                if(!move_uploaded_file($_FILES["image"]["tmp_name"], $imagePath)){
+                    $imageError = "Il y a eu une erreur lors de l'upload";
+                    $isUploadSuccess = false;
+                }
+            }
+        }
+        if($isSuccess && $isUploadSuccess){
+            $db = Database::connect();
+            $statement = $db->prepare("INSERT INTO items (name,description,price,category,image) values(?, ?, ?, ?, ?)");
+            $statement->execute(array($name,$description,$price,$category,$image));
+            Database::disconnect();
+            header("Location: index.php");
+        }
+  }
+
+
+  function checkInput($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 
 ?>
 
@@ -65,11 +133,12 @@
                 <span class="help-inline"><?php echo $imageError; ?></span>
               </div>
 
-          </form>
+          
           <div class="form-actions">
               <button type="submite" class="btn btn-success"><span class="fas fa-pen"> Ajouter</span></button>
               <a class="btn btn-primary" href="index.php"><span class="fas fa-arrow-left"></span> Retour</a>
           </div>
+        </form>
         
       </div>
     </div>
