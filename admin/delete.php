88923a2cbe8f6db0ef3c416510ac2cc22bad57e9
MaksymY <maksym.yankivskyypro@gmail.com>
2019-04-06 14:12:38 +0200

    delet in administator paneel ready

--- admin/delete.php
+++ admin/delete.php
@@ -5,6 +5,15 @@
       $id = checkInput($_GET['id']);
   }
 
+  if(!empty($_POST)){
+      $id = checkInput($_POST['id']);
+      $db = Database::connect();
+      $statement = $db->prepare("DELETE FROM items WHERE id = ?");
+      $statement->execute(array($id));
+      Database::disconnect();
+      header("Location: index.php");
+  }
+
   function checkInput($data){
     $data = trim($data);
     $data = stripslashes($data);
